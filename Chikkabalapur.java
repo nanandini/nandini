197d2729b733af320a4253b3a5a08f77cedaeea7
unknown <nandinin.xworkz@gmail.com>
2022-10-11 11:56:41 +0530

    new

---
+++ Chikkabalapur.java
@@ -0,0 +1,9 @@
+package com.xworkz.copy;
+
+public class Chikkabalapur {
+    public Chikkabalapur()
+    {
+    	System.out.println("native place");
+    }
+}
+
---
+++ Thing.java
@@ -0,0 +1,8 @@
+package com.xworkz.copy;
+
+public class Thing {
+   public Thing()
+   {
+	   System.out.println(" A good heart can able to see ");
+   }
+}
