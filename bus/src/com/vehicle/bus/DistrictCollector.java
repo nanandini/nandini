ef0d8247ce47596b71f30939636c3540c96d9135
unknown <nandinin.xworkz@gmail.com>
2022-10-12 07:49:10 +0530

    hello

---
+++ bus/src/com/vehicle/bus/DistrictCollector.java
@@ -0,0 +1,13 @@
+package com.vehicle.bus;
+
+public class DistrictCollector {
+	 public String name;
+	 public int age;
+	 public String district;
+	 public int batchNo;
+	 
+     public DistrictCollector()
+     {
+    	 System.out.println("responsible for the maintenance of law and order in the districts");
+     }
+}
---
+++ bus/src/com/vehicle/bus/SongRunner.java
@@ -0,0 +1,28 @@
+package com.vehicle.bus;
+
+public class SongRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Song  favList=new Song();
+		
+		favList.name="Yenidu E dina";
+		System.out.println("name :"+favList.name);
+		
+		favList.language="Kannada";
+		System.out.println("language :"+favList.language);
+		
+		favList.singer="Rajesh Krishnan";
+		System.out.println("singer :"+favList.singer);
+		
+		favList.lyrics="Akash";
+		System.out.println("lyrics :"+favList.lyrics);
+		
+		favList.type="feeling song";
+		System.out.println("type :"+favList.type);
+		
+
+	}
+
+}
