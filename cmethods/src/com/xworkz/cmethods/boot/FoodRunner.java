60341e4c2779e7beb78fec21466a792f1411b401
unknown <nandinin.xworkz@gmail.com>
2022-10-19 08:21:00 +0530

    methods

---
+++ cmethods/src/com/xworkz/cmethods/boot/FoodRunner.java
@@ -0,0 +1,19 @@
+package com.xworkz.cmethods.boot;
+
+import com.xworkz.cmethods.ctype.*;
+
+public class FoodRunner {
+
+	public static void main(String[] args) {
+		String[] colours = { "green", "yellow" };
+		String[] foodNames = { "ponagal", "fideRice" };
+		String[] shopNames = { "krishnaShop", "vinayaka" };
+		String[] ownerNames = { "MYSELF", "NANDHINI" };
+		long[] ownerPhoneNumbers = { 9900276144L, 9448958488L };
+		boolean[] serveFacilities = { true, false };
+
+		Food food = new Food("vegiterian", "vegPallav", "vegTypes", true, 35,colours,foodNames,shopNames,ownerNames,ownerPhoneNumbers,serveFacilities);
+		food.display();
+	}
+
+}
---
+++ cmethods/src/com/xworkz/cmethods/ctype/Food.java
@@ -0,0 +1,68 @@
+package com.xworkz.cmethods.ctype;
+
+public class Food {
+	public String name;
+	public String fav;
+	public String type;
+	public boolean good;
+	public int price;
+
+	public String[] colours;
+	public String[] foodNames;
+	public String[] shopNames;
+	public String[] ownerNames;
+	public long[] ownerPhoneNumbers;
+	public boolean[] serveFacilities;
+
+	public Food(String name, String fav, String type, boolean good, int price, String[] colours, String[] foodNames,
+			String[] shopNames, String[] ownerNames, long[] ownerPhoneNumbers, boolean[] serveFacilities) {
+		this.name = name;
+		this.fav = fav;
+		this.type = type;
+		this.good = good;
+		this.price = price;
+		this.colours = colours;
+		this.foodNames = foodNames;
+		this.shopNames = shopNames;
+		this.ownerNames = ownerNames;
+		this.ownerPhoneNumbers = ownerPhoneNumbers;
+		this.serveFacilities = serveFacilities;
+
+	}
+
+	public Food() {
+
+	}
+
+	public void display()
+
+	{
+		System.out.println("name:" + this.name);
+		System.out.println("fav:" + this.fav);
+		System.out.println("type:" + this.type);
+		System.out.println("good:"+this.good);
+		System.out.println("price:"+this.price);
+
+		for (int i = 0; i < colours.length; i++) {
+			System.out.println(this.colours[i]);
+		}
+		System.out.println(System.lineSeparator() + "foodNames");
+		for (int i = 0; i < foodNames.length; i++) {
+			System.out.println(this.foodNames[i]);
+		}
+		for (int i = 0; i < serveFacilities.length; i++) {
+			System.out.println(this.serveFacilities[i]);
+		}
+		for (int i = 0; i < shopNames.length; i++) {
+			System.out.println(this.shopNames[i]);
+		}
+		for (int i = 0; i < ownerNames.length; i++) {
+			System.out.println(this.ownerNames[i]);
+		}
+		for (int i = 0; i < ownerPhoneNumbers.length; i++) {
+			System.out.println(this.ownerPhoneNumbers[i]);
+			
+		}
+	}
+
+}
