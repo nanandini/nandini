2b3bca3ce8f443d28f76ed8450126fdbbb4c73fe
unknown <nandinin.xworkz@gmail.com>
2022-12-06 22:36:31 +0530

    exception task

---
+++ exceptionTask/src/com/xworkz/exceptionTask/eventRunner/ExceptionRunner.java
@@ -0,0 +1,134 @@
+package com.xworkz.exceptionTask.eventRunner;
+
+import java.awt.AWTException;
+import java.awt.FontFormatException;
+import java.awt.datatransfer.MimeTypeParseException;
+import java.awt.geom.NoninvertibleTransformException;
+import java.beans.IntrospectionException;
+import java.io.IOException;
+import java.lang.instrument.IllegalClassFormatException;
+import java.rmi.activation.ActivationException;
+import java.security.GeneralSecurityException;
+import java.security.acl.AclNotFoundException;
+import java.security.acl.LastOwnerException;
+import java.security.cert.CertificateException;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.zip.DataFormatException;
+
+import javax.management.BadAttributeValueExpException;
+import javax.management.BadBinaryOpValueExpException;
+import javax.management.BadStringOperationException;
+import javax.management.InvalidApplicationException;
+import javax.management.modelmbean.InvalidTargetObjectTypeException;
+import javax.naming.NamingException;
+import javax.security.auth.DestroyFailedException;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.swing.text.BadLocationException;
+import javax.swing.tree.ExpandVetoException;
+import javax.xml.crypto.KeySelectorException;
+import javax.xml.datatype.DatatypeConfigurationException;
+
+import org.ietf.jgss.GSSException;
+import org.omg.CORBA.portable.ApplicationException;
+
+import com.xworkz.exceptionTask.event.Exceptions;
+
+public class ExceptionRunner {
+
+	public static void main(String[] args) throws AclNotFoundException, ActivationException, ApplicationException,
+			AWTException, BadAttributeValueExpException, BadBinaryOpValueExpException, BadLocationException,
+			BadStringOperationException, BrokenBarrierException, CloneNotSupportedException, DestroyFailedException,
+			DataFormatException, DatatypeConfigurationException, ExpandVetoException, FontFormatException, GSSException,
+			IllegalClassFormatException, GeneralSecurityException, InterruptedException, IntrospectionException,
+			InvalidApplicationException, InvalidMidiDataException, InvalidTargetObjectTypeException, IOException,
+			NoninvertibleTransformException, MimeTypeParseException, NamingException, MidiUnavailableException,
+			LineUnavailableException, LastOwnerException, KeySelectorException {
+
+		Exceptions except = new Exceptions();
+		except.method1();
+		except.method2();
+		except.method3();
+		except.method4();
+		except.method5();
+		except.method6();
+		except.method7();
+		except.method8();
+		except.method9();
+		except.method10();
+		except.method11();
+		except.method12();
+		except.method14();
+		except.method15();
+		except.method16();
+		except.method17();
+		except.method18();
+		except.method19();
+		except.method20();
+		except.method21();
+		except.method22();
+		except.method23();
+		except.method24();
+		except.method25();
+		except.method26();
+		except.method27();
+		except.method28();
+		except.method30();
+		except.method31();
+		except.method32();
+		except.method33();
+		except.method34();
+		except.method35();
+		except.method36();
+		except.method37();
+		except.method37();
+		except.method38();
+		except.method39();
+		except.method40();
+		except.method41();
+		except.method42();
+		except.method43();
+		except.method44();
+		except.method45();
+		except.method46();
+		except.method47();
+		except.method48();
+		except.method49();
+		except.method50();
+		except.method51();
+		except.method52();
+		except.method53();
+		except.method54();
+		except.method55();
+		except.method56();
+		except.method57();
+		except.method58();
+		except.method59();
+		except.method60();
+		except.method61();
+		except.method62();
+		except.method63();
+		except.method64();
+		except.method65();
+		except.method66();
+		except.method67();
+		except.method68();
+		except.method69();
+		except.method70();
+		except.errorMethod1();
+		except.errorMethod2();
+		except.errorMethod3();
+		except.errorMethod4(null);
+		except.errorMethod5();
+		except.errorMethod6(null);
+		except.errorMethod7();
+		except.errorMethod8();
+		except.errorMethod9();
+		except.errorMethod10();
+		except.errorMethod11();
+		except.errorMethod12();
+
+	}
+
+}
